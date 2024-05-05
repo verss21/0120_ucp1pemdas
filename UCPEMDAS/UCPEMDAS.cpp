a5e9ffa1a28de6acac58fd64422481673cfcb75f
verss21 <arjunafrizal01@gmail.com>
2024-05-05 12:22:07 +0700

    Add project files.

---
+++ UCPEMDAS/UCPEMDAS.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	float nSensor1, nSensor2, nSensor3, rerata;
+	string status
+
+		cout << "Masukan nilai sensor 1"
+		cin >> nSensor1;
+	cout << "Masukan nilai sensor 2"
+		cin >> nSensor2;
+	cout << "Masukan nilai sensor 3"
+		cin >> nSensor3;
+
+	rerata = (nSensor1 = nSensor2 = nSensor3) / 3;
+
+	if (rerata >= 75) {
+		status = "Tidak Sehat"
+	}
+	else if (nSensor1, nSensor2, nSensor3 >= 75) {
+		status = "Tidak Sehat";
+	}
+	else
+	{
+		status = "Sehat"
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// deklarasi variable string,int,float
+	// langkah langkah menyelesaikan 1 masalah saja
+	// menyelesaikan masalah dengan tipe data int seperti bilangan bulat
+	// menentukan nilai rerata dari 2 kondisi
+	// 
